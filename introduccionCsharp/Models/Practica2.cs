55aea244cde9697eb7c3ad1deac1d136a067f2f0
DIEGO ALEJANDRO GONZALEZ GAYTAN <c4p9jyqcsm@privaterelay.appleid.com>
2026-05-06 21:01:30 -0600

    Modelos 06/05/2026

---
+++ introduccionCsharp/Models/Practica2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace introduccionCsharp.Models
+{
+    /*
+     * class[nombre] {
+     * [atributos o variables]
+     * [métodos o funciones]
+     * }
+     */
+{
+    class Practica2
+    {
+        string nombre;
+        private int edad;
+        protected string carrera;
+
+        public void IngresarDatos()
+        {
+            Console.WriteLine("Ingresa tu nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingresa tu edad: ");
+            edad = Console.ReadLine();
+
+            Console.WriteLine("Ingresa tu carrera");
+            carrera = Console.ReadLine();
+        }
+
+        public void MostrarDatos()
+        {
+            Console.WriteLine("Nombre: " + nombre);
+            Console.WriteLine("Edad: " + edad);
+            Console.WriteLine("Carrera: " + carrera);
+        }
+
+        static void Main(string[] args)
+        {
+            Practica2 p2 = new Practica2();
+            p2.IngresarDatos();
+            p2.MostrarDatos();
+        }
+    }
+}
